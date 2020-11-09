f6f2188ab0ef84e08bea8528c732f92bd8187941
Natalya Ryabaya <natalir.80@gmail.com>
2020-11-09 23:10:30 +0300

    changed RadioTest

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -36,4 +36,37 @@ class RadioTest {
         radio.decreaseCurrentVolume();
         assertEquals(0, radio.getCurrentVolume());
     }
+    @Test
+    public void CurrentStationAboveMaxStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+        radio.increaseCurrentStation();
+        radio.increaseCurrentStation();
+
+        assertEquals(1, radio.getCurrentStation());
+    }
+    @Test
+    public void CurrentStationBelowMin(){
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+        radio.decreaseCurrentStation();
+        radio.decreaseCurrentStation();
+        assertEquals(8, radio.getCurrentStation());
+    }
+    @Test
+    public void CurrentVolumeAboveMax() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(10);
+        radio.increaseCurrentVolume();
+        radio.increaseCurrentVolume();
+        assertEquals(10, radio.getCurrentVolume());
+    }
+    @Test
+    public void CurrentVolumeBelowMin(){
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        radio.decreaseCurrentVolume();
+        radio.decreaseCurrentVolume();
+        assertEquals(0, radio.getCurrentVolume());
+    }
 }
